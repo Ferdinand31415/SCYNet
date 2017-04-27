cdacbffb7c581dc95e7fd3b44af283fccb3cd767
Ferdinand31415 <Ferdinand.Eiteneuer@rwth-aachen.de>
2017-04-27 17:46:30 +0200

    pmssm classifier

--- classifier.py
+++ classifier.py
@@ -4,47 +4,74 @@ from keras.regularizers import l2
 from keras.optimizers import SGD, Adam, Nadam, Adagrad
 #from keras.layers.normalization import BatchNormalization
 from keras.callbacks import History, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+import numpy as np
+import sys
+import keras.backend as K
 
+data = '/scratch/work/eiteneuer/data/numpy_data_pmssm'
+data = '/home/fe918130'
 
-
-
-x = np.load('/scratch/work/eiteneuer/data/numpy_data_pmssm/pmssm.npy')
-y = np.load('/scratch/work/eiteneuer/data/numpy_data_pmssm/chi2_binary.npy')
+x = np.load(data +'/pmssm.npy')
+y = np.load(data +'/chi2_binary.npy')
+y = y/100.0
 
 split = int(len(x)*6.0/8)
-x_test = x[:split]
-x_train = x[split:]
+x_test = x[split:]
+x_train = x[:split]
 
-y_test = x[:split]
-y_train = x[split:]
+y_test = y[split:]
+y_train = y[:split]
 
+from keras.utils import np_utils
+y_train_ohe = np_utils.to_categorical(y_train)
+y_test_ohe = np_utils.to_categorical(y_test)
+
+import sys
+#sys.exit()
 mod = []
-for i in x.shape[1]:
+for i in range(x.shape[1]):
     std = x[:,i].std()
     mean = x[:,i].mean()
     x[:,i] = (x[:,i]-mean)/std
-    mod.append(mean,std)
+    mod.append([mean,std])
 
 
 model = Sequential()
-n, act = 100, 'relu'
-model.add(Dense(n, init='glorot_uniform',
-#		init='zero',
+n, act = 1500, 'relu'
+model.add(Dense(n, kernel_initializer='glorot_uniform',
+#		kernel_initializer='zero',
 		activation=act,
-		input_dim=x.train.shape[1]))
+		input_dim=11))
 for i in range(3):
-    model.add(Dense(n-0*i, init='glorot_uniform',activation=act))#, W_regularizer=l2(0.001)))
-    model.add(Dropout(0.08))
+    model.add(Dense(n-0*i, kernel_initializer='glorot_uniform',activation=act, kernel_regularizer=l2(0.001)))
+    model.add(Dropout(0.1))
 
-model.add(Dense(1, init='tanh',activation=act))
-
-
-def trainnadam(lr=1e-1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004,n=1,b=500,loss='binary_crossentropy'):
-	nadam = Nadam(lr=lr, beta_1=beta_1, beta_2=beta_2, epsilon=epsilon, schedule_decay=schedule_decay)
-	model.compile(loss = loss, optimizer = nadam, metrics=['accuracy'])
-	model.fit(x_train,y_train, validation_data =(x_test, y_test), epochs = n,batch_size=b)
-
-trainnadam(lr=10**(-3.5),n=5,schedule_decay=1e-2)
+model.add(Dense(1, kernel_initializer='glorot_uniform',activation='sigmoid'))
 
 
+#opt = SGD(lr=10**(-2.55555), momentum=0.9, decay=0, nesterov=True)
+opt = Nadam(lr=10**(-2.5), beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0)
+early_stopping = EarlyStopping(monitor='val_loss', patience=13, mode='min', verbose=1)
+modcp = ModelCheckpoint("bestclassifier.hdf5", monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+history = History()
+histos = []
+lr=10**(-3.5)
+if len(sys.argv) > 1:
+    lr = 10**(float(sys.argv[-1]))
 
+model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+while lr > 10**(-7.1):
+    K.set_value(model.optimizer.lr, lr)
+    history = History()
+    model.fit(x_train, y_train, validation_data=(x_test,y_test), epochs=400, batch_size=300, verbose=1, callbacks=[history,early_stopping,modcp])
+    model.load_weights('bestclassifier.hdf5')
+    lr /= 4.0
+    print 'lr:', lr
+    histos.append(history)
+
+#reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.66, patience=13, min_lr=10**(-6.0), verbose=1)
+
+
+#model.compile(loss = 'binary_crossentropy', optimizer = opt, metrics=['accuracy'])
+#model.fit(x_train,y_train, validation_data =(x_test, y_test), epochs = n,batch_size=1000, callbacks = [reduce_lr, modcp, history])
+#'''
--- pmssm.py
+++ pmssm.py
@@ -7,6 +7,23 @@ from keras.optimizers import SGD, Adam, Nadam, Adagrad
 #from keras.layers.normalization import BatchNormalization
 from keras.callbacks import History, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
+def only_small_chi2(path='/home/fe918130/13TeV_chi2_disjoint_2',split=0.8):
+    d=np.genfromtxt(path)[:,1:]
+    p=d[:,:-1]
+    chi2 = d[:,-1]
+    mask = chi2 < 100
+    p=p[mask]
+    chi2 =chi2[mask]
+    chi2 = chi2/100
+    for i in range(p.shape[1]):
+        mean,std=p[:,i].mean(), p[:,i].std()
+        p[:,i] = (p[:,i] - mean)/std
+    split = int(len(chi2)*split)
+    x_train = p[:split]
+    y_train = chi2[:split]
+    x_test = p[split:]
+    y_test = chi2[split:]
+    return x_train, x_test, y_train, y_test
 
 from copy import deepcopy
 
@@ -22,15 +39,15 @@ x = pmssm(data.data[:,:-1], preproc = ['log_norm','min_max'], split = split)
 y = chi2(data.data[:,-1], preproc = ['square_cut','div_max'], params = [100,25], split= split)
 
 model = Sequential()
-n, act = 100, 'relu'
-model.add(Dense(n, init='glorot_uniform',
-#		init='zero',
+n, act = 300, 'relu'
+model.add(Dense(n, kernel_initializer='glorot_uniform',
+#		kernel_initializer='zero',
 		activation=act,
 		input_dim=x.train.shape[1]))
 for i in range(3):
-    model.add(Dense(n-0*i, init='glorot_uniform',activation=act))#, W_regularizer=l2(0.001)))
-    model.add(Dropout(0.08))
-model.add(Dense(1, init='glorot_uniform',activation=act))
+    model.add(Dense(n-0*i, kernel_initializer='glorot_uniform',activation=act))#, W_regularizer=l2(0.001)))
+    model.add(Dropout(0.05))
+model.add(Dense(1, kernel_initializer='glorot_uniform',activation=act))
 
 
 import keras.backend as K
@@ -50,7 +67,7 @@ def trainnadam(lr=1e-1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=
 	y.evaluation(x,model)
 
 #Define Early Stopping(If val_loss doeasn't decrease in 50 epochs stop)
-early_stopping = EarlyStopping(monitor='val_loss', patience=3, mode='min', verbose=1)
+early_stopping = EarlyStopping(monitor='val_loss', patience=18, mode='min', verbose=1)
 
 #Save the model if new val_los minimum
 modcp = ModelCheckpoint("bestnet.hdf5", monitor='val_loss', verbose=1, save_best_only=True, mode='min')
@@ -59,26 +76,43 @@ modcp = ModelCheckpoint("bestnet.hdf5", monitor='val_loss', verbose=1, save_best
 history = History()
 
 #Main Lerning Loop. Start with standard Learning rate, then train still val_loss stops decreasing. Then decrease learning rate and train again.
-'''
-learnrate=10**(-3.0)
-while learnrate > 10**(-4.1):
+
+
+#'''
+learnrate=10**(-2.8)
+while learnrate > 10**(-7.1):
 	opt = Nadam(lr=learnrate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0)
-	model.compile(loss='mse', optimizer=opt, metrics=[mean_error_chi2])
+	model.compile(loss='mae', optimizer=opt, metrics=[mean_error_chi2])
 	model.fit(x.train, y.train, validation_data=(x.test,y.test), epochs=400, batch_size=1000, verbose=1, callbacks=[history,early_stopping,modcp])
-	model.load_weights('best_net_r_chi2.hdf5')
-	learnrate /= 5
+	model.load_weights('bestnet.hdf5')
+	learnrate /= 4
+	print 'learnrate:', learnrate
+#'''
+
+
+'''
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.66, patience=13, min_lr=10**(-6.0), verbose=1)
+
+opt = Nadam(lr=10**(-2.8), beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0)
+#opt = SGD(lr=10**(-2.55555), momentum=0.9, decay=0)
+
+model.compile(loss = 'mae', optimizer = opt, metrics=[mean_error_chi2])
+model.fit(x.train,y.train, validation_data =(x.test, y.test), epochs = n,batch_size=1000, callbacks = [reduce_lr, modcp, history])
+y.evaluation(x, model)
+'''
+import sys
+'''
+x_train, x_test, y_train, y_test = only_small_chi2()
+#sys.exit()
+learnrate=10**(-2.8)
+while learnrate > 10**(-7.1):
+	opt = Nadam(lr=learnrate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0)
+	model.compile(loss='mae', optimizer=opt, metrics=[mean_error_chi2])
+	model.fit(x_train, y_train, validation_data=(x_test,y_test), epochs=400, batch_size=1000, verbose=1, callbacks=[history,early_stopping,modcp])
+	model.load_weights('bestnet.hdf5')
+	learnrate /= 4
 	print 'learnrate:', learnrate
 '''
 
-
-#'''
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=10**(-4.1), verbose=1)
-nadam = Nadam(lr=10**(-3), beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0)
-model.compile(loss = 'mae', optimizer = nadam, metrics=[mean_error_chi2])
-model.fit(x.train,y.train, validation_data =(x.test, y.test), epochs = n,batch_size=1000, callbacks = [reduce_lr, modcp, history])
-y.evaluation(x, model)
-#'''
-
-
-#trainsgd(l=10**(-2.2),mom=0.2,n=10)
 #trainnadam(lr=10**(-3.5),n=5,schedule_decay=1e-2)
+#trainsgd(l=10**(-2.2),mom=0.2,n=10)
