ab7cb5c03d3e6b29a79a3342ee4ba8a4456f9030
Ferdinand31415 <ferdinand.eiteneuer@rwth-aachen.de>
2017-04-28 11:45:13 +0200

    ..

--- pmssm.py
+++ pmssm.py
@@ -7,6 +7,7 @@ from keras.optimizers import SGD, Adam, Nadam, Adagrad
 #from keras.layers.normalization import BatchNormalization
 from keras.callbacks import History, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
+import keras.backend as K
 '''
 def only_small_chi2(path='/home/fe918130/13TeV_chi2_disjoint_2',split=0.8):
     d=np.genfromtxt(path)[:,1:]
@@ -27,7 +28,6 @@ def only_small_chi2(path='/home/fe918130/13TeV_chi2_disjoint_2',split=0.8):
     return x_train, x_test, y_train, y_test
 '''
 from copy import deepcopy
-
 from Preprocessor import pmssm, chi2, shuffle_data, fulldata
 
 data = fulldata()
@@ -51,40 +51,22 @@ for i in range(3):
 model.add(Dense(1, kernel_initializer='glorot_uniform',activation=act))
 
 
-import keras.backend as K
-'''
-def mean_error_chi2(y_true, y_pred):
-    return 100*K.mean(K.abs(y_pred-y_true))
-
-def trainsgd(l=1e-1, mom=0.9, dec=1e-4, nes=False,n=3,b=500,loss='mse'):
-	sgd = SGD(lr=l, momentum=mom, decay=dec, nesterov=nes)
-	model.compile(loss = loss, optimizer = sgd)
-	model.fit(x.train,y.train,nb_epoch = n,batch_size=b)
-	d.eval(model)
-
-def trainnadam(lr=1e-1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004,n=1,b=500,loss='mse'):
-	nadam = Nadam(lr=lr, beta_1=beta_1, beta_2=beta_2, epsilon=epsilon, schedule_decay=schedule_decay)
-	model.compile(loss = loss, optimizer = nadam, metrics=[mean_error_chi2])
-	model.fit(x.train,y.train, validation_data =(x.test, y.test), epochs = n,batch_size=b)
-	y.evaluation(x,model)
-'''
 
 early_stopping = EarlyStopping(monitor='val_loss', patience=18, mode='min', verbose=1)
 modcp = ModelCheckpoint("bestnet.hdf5", monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 history = History()
 
-#'''
 learnrate=10**(-2.8)
+opt = Nadam(lr=learnrate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0)
+model.compile(loss='mae', optimizer=opt)
 while learnrate > 10**(-7.1):
-	opt = Nadam(lr=learnrate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0)
-	model.compile(loss='mae', optimizer=opt, metrics=[mean_error_chi2])
+        K.set_value(model.optimizer.lr, lr)
 	model.fit(x.train, y.train, validation_data=(x.test,y.test), epochs=400, batch_size=1000, verbose=1, callbacks=[history,early_stopping,modcp])
 	model.load_weights('bestnet.hdf5')
 	learnrate /= 4
 	print 'learnrate:', learnrate
-#'''
 
-
+y.evaluation(x, model)
 '''
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.66, patience=13, min_lr=10**(-6.0), verbose=1)
 
@@ -94,9 +76,9 @@ opt = Nadam(lr=10**(-2.8), beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_dec
 model.compile(loss = 'mae', optimizer = opt, metrics=[mean_error_chi2])
 model.fit(x.train,y.train, validation_data =(x.test, y.test), epochs = n,batch_size=1000, callbacks = [reduce_lr, modcp, history])
 y.evaluation(x, model)
-'''
+
 import sys
-'''
+
 x_train, x_test, y_train, y_test = only_small_chi2()
 #sys.exit()
 learnrate=10**(-2.8)
@@ -108,6 +90,3 @@ while learnrate > 10**(-7.1):
 	learnrate /= 4
 	print 'learnrate:', learnrate
 '''
-
-#trainnadam(lr=10**(-3.5),n=5,schedule_decay=1e-2)
-#trainsgd(l=10**(-2.2),mom=0.2,n=10)
